1984bc2fdab9e3adbd9d73451197e0614cd6f50c
Shutaro Takimoto <shutaro_takimoto@dwango.co.jp>
2016-02-03 14:22:31 +0000

    lot> に反応するおみくじ機能を追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,9 +1,16 @@
 (function () {
-	'use strict';
-	module.exports = (robot) => {
-		robot.hear(/hello>/i, (msg) => {
-			let username = msg.message.user.name;
-			msg.send('Hello, ' + username);
-		});
-	};
+    'use strict';
+    module.exports = (robot) => {
+        robot.hear(/hello>/i, (msg) => {
+            let username = msg.message.user.name;
+            msg.send('Hello, ' + username);
+        });
+        robot.hear(/lot>/i, (msg) => {
+            let username = msg.message.user.name;
+            const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+            let lot = lots[Math.floor(Math.random() * lots.length)];
+            
+            msg.send(lot + ', ' + username);
+        });
+    };
 })();
