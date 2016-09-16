68fb21d83853ba80b6c6be0a8b8f74ebe1fa40a3
ninja6331 <ninja6331@gmail.com>
2016-09-16 12:57:24 +0000

    おみくじの作成

--- scripts/hello.js
+++ scripts/hello.js
@@ -8,9 +8,16 @@
 		
 		robot.hear(/lot>/i, (msg) => {
 			const username = msg.message.user.name;
-			const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+			const lots = ['超大吉', '大吉', '吉', '中吉', '末吉', '凶', '大凶'];
 			const lot = lots[Math.floor(Math.random() * lots.length)];
 			msg.send(username + ' の今日の運勢は・・・ ' + lot + ' でした!!');
+		});
+
+		robot.hear(/looot>/i, (msg) => {
+			const username = msg.message.user.name;
+			const looots = ['勉強しなさい', '5分休憩', '解散!'];
+			const looot = looots[Math.floor(Math.random() * looots.length)];
+			msg.send(username + ' は ' + looot);
 		});
 	};
 })();
