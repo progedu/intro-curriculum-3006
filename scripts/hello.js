f6b4f5151e248021dbcd56f364701ab6522ccb4c
Yukimasa Shimada <github@eleos.jp>
2016-09-16 12:57:17 +0000

    おみくじの作成

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,29 +1,43 @@
+// ラッキーカラー診断で使う
+function doubleDigots(num){
+    if (num < 16) return "0" + num.toString(16);
+    else return num.toString(16);
+}
+
 (function () {
     'use strict';
+    　//　あいさつ機能 
     module.exports = (robot) => {
         robot.hear(/hello>/i, (msg) => {
             const username = msg.message.user.name;
             msg.send('Hello, ' + username);
         });
+
+      // おみくじ機能  
         robot.hear(/lot>/i, (msg) => {
             const username = msg.message.user.name;
-            const lots = ['大吉', '吉', '中吉', '末吉', '凶'
-/*
-		'タイヤがいつもより丸い',
-		'新大陸を発見',
-		'バスの後ろをおかんが泣きながら走ってきてもう3県目になる',
-		'落とし穴に落ちつつ',
-		'ゴムの力で帰宅' ,
-		'「このじゃらん何もいい案内載ってないなぁ」と思ってたら じゃらんじゃなく俺の姉ちゃんの商業高校の卒アルだった',
-		'キノコのおいしさに笑いが止まらない',
-		'玄関を出て一歩目で靴ヒモが全部切れた',
-		'交換日記がいつも一日で終わる',
-		'旅館の売店に「◯◯へ行ってきました」っていう菓子しかない',
-		'ごめんなさい lot> の時どんなコメントしたら良いかわからないの'
-*/
-            ];
+            const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
             const lot = lots[Math.floor(Math.random() * lots.length)];
             msg.send(lot + ', ' + username);
         });
+  
+     　// ラッキーカラー診断
+        robot.hear(/color>/i, (msg) => {
+            const username = msg.message.user.name;
+           const r = Math.floor(Math.random() * 255 );
+            const g = Math.floor(Math.random() * 255 );
+            const b = Math.floor(Math.random() * 255 );
+            msg.send(username + 'さんのラッキーカラーは: #' + doubleDigots(r) + doubleDigots(g) + doubleDigots(b) );
+        });
+        
+       // エヴァのMAGIシステムもどき
+        robot.hear(/magi>/i, (msg) => {
+            const username = msg.message.user.name;
+            const lots = ['承認', '否定'];
+            const merchior = lots[Math.floor(Math.random() * lots.length)];
+            const balthasar = lots[Math.floor(Math.random() * lots.length)];
+            const casper = lots[Math.floor(Math.random() * lots.length)];
+            msg.send( ' >' + username + '\r\nMERCHIOR--1 : '+ merchior + '\r\nBALTHASAR-2 : ' + balthasar + '\r\nCASPER ------3 : ' + casper ) ;
+       });
     };
 })();
