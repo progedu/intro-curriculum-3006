fc4832cbb142d3922b0993a5bb146e4afccce796
Tsubasa-Sato <tsubasa_16n1200394@nnn.ed.jp>
2016-09-16 12:49:24 +0000

    おみくじ機能を追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -5,6 +5,7 @@ module.exports = (robot) => {
 		msg.send('Hello, ' + username);
 	});
 	robot.hear(/lot>/i, (msg) => {
+		const username = msg.message.user.name;
 		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
 		const lot = lots[Math.floor(Math.random() * lots.length)];
 		msg.send(lot + ',' + username);
