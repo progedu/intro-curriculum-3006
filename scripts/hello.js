d611937f929cf9316a8759198ddb852faa10e828
DDRAGON <davide.0829@gmail.com>
2016-09-16 16:37:23 +0900

    即時実行関数の削除

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,9 +1,7 @@
-(function () {
-	'use strict';
-	module.exports = (robot) => {
-		robot.hear(/hello>/i, (msg) => {
-			const username = msg.message.user.name;
-			msg.send('Hello, ' + username);
-		});
-	};
-})();
+'use strict';
+module.exports = (robot) => {
+	robot.hear(/hello>/i, (msg) => {
+		const username = msg.message.user.name;
+		msg.send('Hello, ' + username);
+	});
+};
