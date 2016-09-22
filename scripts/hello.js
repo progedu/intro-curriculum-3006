ed9c14ce2cd13813f28b7d1585cde7dc0ecd2fae
kanijpn <masahiro@okatani.com>
2016-09-22 03:09:07 +0000

    ボットにおみくじ追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,10 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+	robot.hear(/lot>/i, (msg) => {
+		const username = msg.message.user.name;
+		const lots = ['大吉','吉','中吉','末吉','凶'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send(lot + ', ' + username);
+	});
 };
