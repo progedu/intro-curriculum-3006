4fb70396ce8570bd9c85229ae55f08c1200caa99
mskblur <muneo551@gmail.com>
2016-12-02 14:26:16 +0900

    おみくじbot の実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -3,5 +3,11 @@ module.exports = (robot) => {
 	robot.hear(/hello>/i, (msg) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
-	});
+    });
+	robot.hear(/lot>/i, (msg) => {
+		const username = msg.message.user.name;
+        let lot = ['大吉', '吉', '中吉', '末吉', '凶'];
+        let r = Math.floor(Math.random()*5);
+        msg.send(`${lot[r]}, ` + username);
+    });
 };
