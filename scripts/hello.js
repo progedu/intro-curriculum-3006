0c243ba36199870248ae46096e54dd9b5a77cc7e
konkiyo <kontonwa@gmail.com>
2016-12-16 07:53:22 +0000

    占いを追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -2,6 +2,12 @@
 module.exports = (robot) => {
 	robot.hear(/hello>/i, (msg) => {
 		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
+		msg.send('Hell, ' + username);
+	});
+
+	robot.hear(/lot>/i, (msg) => {
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+ 		msg.send('今日の占い, ' + lot);
 	});
 };
