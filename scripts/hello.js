db1a7af3b60aa2af48f57bb5773557acf5102b72
200lux <tithrx@gmail.com>
2017-02-03 13:59:49 +0000

    bot におみくじを引いてもらう lot 関数を実装しました

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,10 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+	robot.hear(/lot>/i, (msg) => {
+		const username = msg.message.user.name;
+        const lots = ['大吉', '吉', '中吉', '小吉', '凶', '末吉', '吉凶未分', '吉凶交々'];
+        const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send('Today\'s fortune of ' + username + ' is ' + lot);
+	});
 };
