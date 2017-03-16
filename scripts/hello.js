0883b2d013f5767f77d0082a58ebdd823c05d2d1
soukawa0816 <soukawa0816@gmail.com>
2017-03-16 23:37:54 +0900

    komentouttuika

--- scripts/hello.js
+++ scripts/hello.js
@@ -8,6 +8,9 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
 		const lot = lots[Math.floor(Math.random()*lots.length)];
+		//math.random()--0から１までランダム
+		//lots.length (配列の長さをかける)
+		//math.floor 小数点以下全部切っちゃう
 		msg.send( lot + ',' + username);
 	});
 };
