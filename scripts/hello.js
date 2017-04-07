ab1380ce62627ef4e04085000d748579d5570c89
awake-murabito <yuusuke@koitabashiyuusukenoMacBook.local>
2017-04-07 22:55:51 +0900

    ユーザーネームの表示を忘れてたので追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -7,6 +7,6 @@ module.exports = (robot) => {
 	robot.hear(/lot>/i, (msg) => {
 		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
 		const lot = lots[Math.floor(Math.random() * lots.length)];
-		msg.send('今日の運勢は... ' + lot + '!');
+		msg.send('今日の運勢は... ' + lot + '!' + username);
 	});
 };
