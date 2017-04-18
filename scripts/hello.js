de959b52fbaedfc0525cabd9d98f13aa77e851ab
lakusui <vau.qi.55@gmail.com>
2017-04-18 04:29:48 +0000

    おみくじ機能の追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,10 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+	robot.hear(/lot>/i, (msg) => {
+		const username = msg.message.user.name;
+		const lots =['大吉','吉','中吉','末吉','凶'];
+		const lot = lot[Math.floor(Math.randam() * lots.length)];
+		msg.send(lot + ',' + username);
+	});
 };
