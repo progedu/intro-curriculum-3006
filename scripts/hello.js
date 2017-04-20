5b1ebafb0e31c41d5eccdda1c1107350887070fc
shirokki <ka18iton@gmail.com>
2017-04-20 00:45:13 +0000

    おみくじの作成

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,20 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+
+	robot.hear(/lot>/i, (msg) => {
+		const username = msg.message.user.name
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send(lot + ', ' + username);
+	});
+
+	robot.hear(/"お昼"/i, (msg) => {
+		const username = msg.message.user.name
+		const lots = ['カレー', '魚', '牛丼', 'ガスト', 'サイゼリヤ'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send(lot + ', ' + username);
+	});
+	
+	
 };
