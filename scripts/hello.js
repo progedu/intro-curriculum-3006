718bafd05b529314b17345e98d713ba442dda620
knzk <knzkfree@gmail.com>
2017-06-11 12:10:07 +0000

    おみくじ実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,11 @@
 'use strict';
+//lot> と入力すると、占い結果を毎回ランダムに返す
 module.exports = (robot) => {
-	robot.hear(/hello>/i, (msg) => {
+	robot.hear(/lots>/i, (msg) => {
 		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+		//lots配列数の範囲でランダムに数字の整数を返す
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send(lot + ', ' + username);
 	});
 };
