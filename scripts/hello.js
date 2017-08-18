c34142c69d1d9b6ee76221c80cc1bdfc48b3cb44
danboor <danboorlife@gmail.com>
2017-08-18 16:24:59 +0000

    おみくじの作成

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,13 @@
 'use strict';
 module.exports = (robot) => {
-	robot.hear(/hello>/i, (msg) => {
-		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
-	});
+    robot.hear(/hello>/i, (msg) => {
+        const username = msg.message.user.name;
+        msg.send('Hello, ' + username);
+    });
+    robot.hear(/lot>/i, (msg) => {
+        const username = msg.message.user.name;
+        const lots = ['大吉', '中吉', '吉', '小吉', '凶', 'さいたま', ];
+        const lot = lots[Math.floor(Math.random() * lots.length)];
+        msg.send(lot + ", " + username);
+    });
 };
