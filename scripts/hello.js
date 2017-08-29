c5c1dd046efbe18e352693c09aa2b3e59baa73a0
208-209 <208_209@outlook.jp>
2017-08-29 19:31:06 +0000

    おみくじの実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,12 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+
+	robot.hear(/lot>/i, (msg) => {
+		const username = msg.message.user.name;
+		const lots = ['赤', '青', '黄色', '緑', '紫', '白'];
+		const num = Math.floor(Math.random() * lots.length);
+		const lot = lots[num];
+		msg.send(`今日の${username}のラッキーカラーは${lot}です！`);
+	});
 };
