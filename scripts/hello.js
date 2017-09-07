f65f2663ff9d1046c24a87445724c86674e0a0b6
tobihaze <30406784+tobihaze@users.noreply.github.com>
2017-09-07 21:07:55 +0900

    インデントの調整

--- scripts/hello.js
+++ scripts/hello.js
@@ -10,6 +10,6 @@ module.exports = (robot) => {
 		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
 		const lot = lots[Math.floor(Math.random() * lots.length)];
 		msg.send(username + 'の運勢は、 ' +lot);
-		});
+	});
 
 };
