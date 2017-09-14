05d2916363dadae83ef67666de03f378ef5ae401
m-sayuri2525 <frawreau@yahoo.co.jp>
2017-09-14 08:06:55 +0000

    slackbot に、おみくじ機能を追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,16 @@
 'use strict';
 module.exports = (robot) => {
+
 	robot.hear(/hello>/i, (msg) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+
+	robot.hear(/lot>/i, (msg) => {
+		const username = msg.message.user.name;
+		const lots = ['大吉0', '大吉1', '大吉2', '大吉3', '大吉4'];		//ゆとり教育なので
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send(lot + ' , ' + username);
+	});
+	
 };
