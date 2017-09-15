4ba0696dfa7a83c9901d6751ccd964318fb016ed
Asazuke11 <30408039+Asazuke11@users.noreply.github.com>
2017-09-16 06:13:12 +0900

    hubotにおみくじを追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,11 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+  
+      robot.hear(/omikuji>/i, (msg) => {
+        const username = msg.message.user.name;
+        const lots = ['大吉', '中吉', '吉', '末吉', '凶'];
+        const omikuji = lots[Math.floor(Math.random() * lots.length)];
+        msg.send('今日は' + omikuji + 'だよ, ' + username);
+    });
 };
