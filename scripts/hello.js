ff2f7264b001e966d1626e460fcb8409dbb448f3
rinringo <rintarou_17n1101688@nnn.ed.jp>
2017-09-23 15:18:39 +0000

    おみくじ機能の追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,12 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+
+robot.hear(/今日の運勢/i, (msg) => {
+const username = msg.message.user.name;
+const lots =['超大吉','大吉','中吉','凶','大凶'];
+const lot = lots[Math.floor(Math.random() * lots.length )];
+msg.send(username + '　の今日の運勢は　'　+ lot);
+});
 };
+
