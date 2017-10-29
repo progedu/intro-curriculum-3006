39ed0c5cd25bd9a109abd57c632bd4348a5a8c9f
nyobikou1 <33064602+nyobikou1@users.noreply.github.com>
2017-10-30 01:10:54 +0900

    rennsyuu

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,10 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+	
+	robot.hear(/lot>/i, (msg) => {
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+		const lot = lots[Math.floor(Math.random() * lots.length)]; //0～１の乱数 * lotsの要素数 Math.floorは小数点切捨て
+		msg.send(msg.message.user.name + 'の運勢は：' + lot);
+	});
 };
