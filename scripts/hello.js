4ee24ee30bec7cee633457c73204977d5b25969b
tkynyk <gmelnuko@gmail.com>
2017-11-14 18:29:45 +0000

    おみくじ機能の追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -7,8 +7,8 @@ module.exports = (robot) => {
 
 	robot.hear(/lot>/i, (msg) =>{
 		const username = msg.message.user.name;
-		const lots = ['大吉','吉','中吉','末吉','凶','大凶'];
-		const lot lots[Math.floor(Math.random() * lots.length)];
-        msg.send(lot +',' + username);
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶', '大凶'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+        msg.send(lot + ',' + username);
 	});
 };
