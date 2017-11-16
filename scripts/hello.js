f82be5a87ed7c5caa78a6920a8225df01b1e8152
by26feb <cha202802@gmail.com>
2017-11-16 22:30:24 +0000

    implement lot function

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,9 @@
 'use strict';
 module.exports = (robot) => {
-	robot.hear(/hello>/i, (msg) => {
+	robot.hear(/lot>/i, (msg) => {
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶']
+		const idx = Math.floor(Math.random() * lots.length);
 		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
+		msg.send(`${lots[idx]}, ${username}`);
 	});
 };
