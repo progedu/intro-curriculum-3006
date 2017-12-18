e28078f2f4ea00fbcf363ed7f7ef70e3743f12cd
acua98dh <vbnsibc@yahoo.co.jp>
2017-12-18 11:24:56 +0000

    lotの実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,14 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+	robot.hear(/lot>/i, (msg) => {
+		const username = msg.message.user.name;
+		const lots =['大吉','吉','中吉','末吉','凶'];
+		const lot = lots[Math.floor(Math.random()*lots.length)];
+		const colors =['赤色','青色','黄色','紫色','白色'];
+		const color = colors[Math.floor(Math.random()*colors.length)];
+		msg.send(lot + ', ラッキーカラーは' + color + 'です,' + username);
+	});
+
 };
+
