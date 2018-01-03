610785a0acbd4ceefc6300a3de4af972dba27e90
えむけー <makoto_16n1200102@nnn.ed.jp>
2018-01-03 14:52:59 +0900

    おみくじ実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,20 @@
-'use strict';
-module.exports = (robot) => {
-	robot.hear(/hello>/i, (msg) => {
-		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
-	});
-};
+// Description:
+//   Messing around with the today API.
+// Commands:
+//   hubot hello
+//   hubot lot
+'use strict'
+module.exports = robot => {
+  const userDisplayName = msg =>
+    msg.message.user.profile.display_name || msg.message.user.profile.real_name
+
+  robot.hear(/hello>/i, msg => {
+    msg.send(`Hello, ${userDisplayName(msg)}`)
+  })
+  
+  robot.hear(/lot>/i, msg => {
+    const lots = ['大吉', '吉', '中吉', '末吉', '凶']
+    const lot = lots[Math.floor(Math.random() * lots.length)]
+    msg.send(`${lot}, ${userDisplayName(msg)}`)
+  })
+}
