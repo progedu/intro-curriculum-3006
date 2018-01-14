95b521639a3811d4c468a32904b7391f2e62f290
大山和徳 <ooyamakazunori@ooyamakazunorinoMacBook-Air.local>
2018-01-15 08:12:31 +0900

    answer

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,10 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+	robot.hear(/fortune>/i, (msg) => {
+		const username = msg.message.user.name;
+		const fortunes = ['very good luck','good luck','soso','bad luck','extremely bad luck'];
+		const fortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+		msg.send('Hello, ' + username + 'さんの今日の運勢は' + fortune +　'です。今日も頑張りましょう！' );
+	});
 };
