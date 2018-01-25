823c28712072d59d7e12e3b6278babcb25630022
popotanu <d.kaneko.d@gmail.com>
2018-01-25 23:50:05 +0900

    いつでも大吉

--- scripts/hello.js
+++ scripts/hello.js
@@ -6,7 +6,7 @@ module.exports = (robot) => {
 	});
 	robot.hear(/lot>/i, (msg) => {
 		const username = msg.message.user.name;
-		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+		const lots = ['大吉', 'とても大吉', 'すごく大吉', 'やっぱり大吉', '凶だと思ったら大吉'];
 		const lot = lots[Math.floor(Math.random() * lots.length)];
 		msg.send(lot + "," + username);
 	});
