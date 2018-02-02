6ebd78be32542b0cb93c93ffe7cfd019be7b71f2
yuimn <kobayashi.yui@gmail.com>
2018-02-02 16:26:42 +0000

    おみくじ転職の追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,11 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+	robot.hear(/いま転職するなら>/i, (msg) => {
+		const username = msg.message.user.name;
+		const lots = ['楽天', 'ビズリーチ', 'メルカリ', 'リクルート', 'ヤフー', 'LINE'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send(lot + 'がオススメですよ、' + username);
+	});
+
 };
