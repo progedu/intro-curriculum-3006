7f20dbf27e51c7e59ed66a5e4e8a5675a741eee8
moyasin2018 <moyasin2006@gmail.com>
2018-02-07 23:41:48 +0000

    hubotおみくじ

--- scripts/hello.js
+++ scripts/hello.js
@@ -5,3 +5,12 @@ module.exports = (robot) => {
 		msg.send('Hello, ' + username);
 	});
 };
+
+module.exports = (robot) => {
+	robot.hear(/lot>/i, (msg) => {
+		const lots=['大吉','吉','中吉','末吉','凶',];
+		const lot =lots[Math.floor(Math.random()* lots.length)];
+		const username = msg.message.user.name;
+		msg.send(lot +',' + username);
+	});
+};
