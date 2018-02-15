d7b595bfbde2c8b9a0f9293a15d4f7f4aabb029f
takagai2 <takagai@rightcode.co.jp>
2018-02-15 01:28:43 +0000

    今日の運勢機能をbotに追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,10 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+	robot.hear(/今日の運勢/i, (msg) => {
+		const username = masg.message.user.name;
+		const lots = ['大吉(^o^)', '吉(^_^)', '中吉('_')', '末吉(*_*)', '凶(T_T)'])
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg .send(username + ' の今日の運勢は... ' + lot)
+	});
 };
