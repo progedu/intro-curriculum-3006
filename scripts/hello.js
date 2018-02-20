31756e05cd17216456c79fba28a38a7ffd80658c
enmto <y.enmto@gmail.com>
2018-02-20 04:58:06 +0000

    おみくじbotの実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,8 @@
 'use strict';
 module.exports = (robot) => {
-	robot.hear(/hello>/i, (msg) => {
-		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
+	robot.hear(/lot>/i, (msg) => {
+		const lots = ['大吉','中吉','小吉','吉'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send('あなたの運勢は,' + lot);
 	});
 };
