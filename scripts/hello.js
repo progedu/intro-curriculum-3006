5bc6956b597ac9065a18d387917b859586a027ef
tetsuro-nakmura <nakamur23jp@yahoo.co.jp>
2018-03-07 12:15:09 +0000

    世界のあいさつ追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,13 @@
 'use strict';
 module.exports = (robot) => {
-	robot.hear(/hello>/i, (msg) => {
-		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
-	});
+    robot.hear(/hello>/i, (msg) => {
+        const username = msg.message.user.name;
+        msg.send('Hello, ' + username);
+    });
+    robot.hear(/あいさつ/i, (msg) => {
+        const username = msg.message.user.name;
+        const lots = ['こんにちは', 'ハロー', 'アンニョンハセヨ', 'ニーハオ', 'ぼんじゅーる'];
+        const lot = lots[Math.floor(Math.random() * lots.length)];
+        msg.send(lot + ', ' + username);
+    });
 };
