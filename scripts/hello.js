d3cd0fe26a22b9dc21f61de8be6a85ca3d954504
atsu-okamoto <ktpolice14.1029@gmail.com>
2018-03-31 15:03:18 +0000

    ランダム機能の追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,15 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+	
+	robot.hear(/lot>/i, (msg) => {
+		const username = msg.message.user.name;
+		const lots = ['ホームラン', '三振', '三塁打', '二塁打', '単打','内野フライ','外野フライ','ゴロアウト','四死球'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send(lot + ',' + username);
+	});	
 };
+
+
+
+
