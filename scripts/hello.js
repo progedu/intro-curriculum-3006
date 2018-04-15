b92fa0b1f348d18ed38a5369f0a9186871f9615b
T Matsumoto <02amimi.n.ppk14@gmail.com>
2018-04-15 16:59:22 +0000

    おみくじ機能の実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,18 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+
+	robot.hear(/lot>/i, (msg) => {
+		const username = msg.message.user.name;
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send(lot + ', ' + username);
+	});
+
+	robot.hear(/seiso>/, (msg) => {
+		const username = msg.message.user.name;
+		const mitos = ['わたくしで隠さなきゃ', 'どなたかおるやんけ', '起立!、気をつけ!', '赤子の拳のような'];
+		const mito = mitos[Math.floor(Math.random() * mitos.length)];
+		msg.send(mito + ', ' + username);
+	});
 };
