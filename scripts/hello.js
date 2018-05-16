27b9777d4178eab3ec893002867227d1688a2604
gobori <sagafro.kichigai@gmail.com>
2018-05-16 03:55:50 +0000

    lot>を聞いておみくじを返す機能を追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,10 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+    robot.hear(/lot>/i, (msg) => {
+        const username = msg.message.user.name;
+        let saikoro = Math.floor(Math.random()*5);
+        const unsei = ["大吉","吉","中吉","末吉","凶"];
+        msg.send(unsei[saikoro] +','+ username);
+    });
 };
