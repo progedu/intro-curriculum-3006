c22a83bdd441e9e00acaae93e09276c69c439f4c
Yamamoto Naoto <yamamoto.naoto@itfllc.co.jp>
2018-05-21 05:06:12 +0000

    質問内容の追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,16 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+	robot.hear(/lot>/i, (msg) => {
+		const username = msg.message.user.name;
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+        const lot = lots[Math.floor(Math.random() * lots.length)];
+        msg.send(lot + ', ' + username);
+	});
+	robot.hear(/好きな色は/i, (msg) => {
+		const username = msg.message.user.name;
+		const lots = ['赤', '黄', '黒', '緑', '青'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+        msg.send(lot + ', ' + username);
+	});
 };
