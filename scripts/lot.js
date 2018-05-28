0c54c2bed754765af4500ebe2d7229a0fb8a3770
seiichi3141 <seiichi3141@gmail.com>
2018-05-28 10:25:40 +0000

    lotの実装

---
+++ scripts/lot.js
@@ -0,0 +1,9 @@
+'use strict';
+module.exports = robot => {
+  robot.hear(/lot>/i, msg => {
+    const username = msg.message.user.name;
+    const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+    const lot = lots[Math.floor(Math.random() * lots.length)];
+    msg.send(lot + ', ' + username);
+  });
+};
