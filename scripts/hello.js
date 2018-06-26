c563a40eadac97f960450498f9ed71908042ef6f
carlitowiu <youllneverwalkalone0817@gmail.com>
2018-06-26 10:23:52 +0000

    ラッキーカラー占いも追加しました！

--- scripts/hello.js
+++ scripts/hello.js
@@ -10,4 +10,10 @@ module.exports = (robot) => {
 		const lot = lots[Math.floor(Math.random() * lots.length)];
 		msg.send(username + ' の今日の運勢は ' +  lot + ' です!');
 	});
+	robot.hear(/color>/i, (msg) => {
+		const username = msg.message.user.name;
+		const colors = ['ブルー', 'シルバー', 'ピンク', 'オレンジ', 'パープル']
+		const color = colors[Math.floor(Math.random() * colors.length)];
+		msg.send(username + ' の今日のラッキーカラーは:  ' +  color + ' です!!!!!');
+	});
 };
