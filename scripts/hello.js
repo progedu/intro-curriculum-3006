2a314ee2a2e5b17730dd937e98ed005e28744b42
mbyk <marubayashi@gmail.com>
2018-07-10 16:19:39 +0000

    おみくじ用の正規表現に修正

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = (robot) => {
-	robot.hear(/hello>/i, (msg) => {
+	robot.hear(/lots>/i, (msg) => {
 		const username = msg.message.user.name;
 		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
 		const lot = lots[Math.floor(Math.random() * lots.length)];
