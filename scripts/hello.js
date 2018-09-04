3cf836c3f8472abb104fcc13060ce7aec45c01d9
khiraku <hirakukubota@docomo.ne.jp>
2018-09-04 14:16:38 +0900

    おみくじのメッセージを修正

--- scripts/hello.js
+++ scripts/hello.js
@@ -8,6 +8,6 @@ module.exports = (robot) => {
                 const username = msg.message.user.name;
                 const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
                 const lot =lots[Math.floor(Math.random() * lots.length)];
-                msg.send(lot + username);
+                msg.send(lot + ', ' + username);
         });
 };
