776f7cd0621af702b37a489850cf60a59754cad8
miifriends <miikun114514@gmail.com>
2018-09-10 11:18:33 +0000

    reply に変更

--- scripts/hello.js
+++ scripts/hello.js
@@ -6,6 +6,6 @@ module.exports = (robot) => {
 		const lots = ['大吉', '吉', '中吉', '小吉', '末吉', '凶'];
 		const lot = lots[Math.floor(Math.random() * lots.length)];
 
-		msg.send(lot + ', ' + username);
+		msg.reply(lot + ', ' + username);
 	});
 };
