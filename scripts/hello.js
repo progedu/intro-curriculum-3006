77445a9d656863327183fe6454000cd5a0ff0459
miifriends <miikun114514@gmail.com>
2018-09-10 12:38:21 +0000

    じゃんけん機能を追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -13,4 +13,34 @@ module.exports = (robot) => {
 
 		msg.reply(lot + 'だよ。 ' + username);
 	});
+
+	robot.hear(/janken>(グー|チョキ|パー)/, (msg) => {
+		const jankens = ['グー', 'チョキ', 'パー'];
+		const botJanken = Math.floor(Math.random() * jankens.length);
+		const janken = jankens[botJanken];
+		let myJanken = null;
+		let result = '';
+
+		switch (msg.match[1]) {
+			case 'グー':
+				myJanken = 0;
+				break;
+			case 'チョキ':
+				myJanken = 1;
+				break;
+			case 'パー':
+				myJanken = 2;
+				break;
+		}
+
+		if (myJanken === botJanken) {
+			result = 'あいこだよ！';
+		} else if ((myJanken + 1) % jankens.length === botJanken) {
+			result = 'あなたの勝ちだよ！';
+		} else {
+			result = 'あなたの負けだよ！';
+		}
+
+		msg.reply(janken + ' ' + result);
+	});
 };
