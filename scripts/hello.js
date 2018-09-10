777080381fba379d54336c330e88556f88b3b744
miifriends <miikun114514@gmail.com>
2018-09-10 11:20:26 +0000

    返すメッセージを変更

--- scripts/hello.js
+++ scripts/hello.js
@@ -6,6 +6,6 @@ module.exports = (robot) => {
 		const lots = ['大吉', '吉', '中吉', '小吉', '末吉', '凶'];
 		const lot = lots[Math.floor(Math.random() * lots.length)];
 
-		msg.reply(lot + ', ' + username);
+		msg.reply(lot + 'だよ。 ' + username);
 	});
 };
