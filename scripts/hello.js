957e7d3b6453f6b9ee9ff223c56367c806a84b0b
maguro777R <41770633+maguro777R@users.noreply.github.com>
2018-09-10 15:10:42 +0900

    おみくじの追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,15 @@
 'use strict';
 module.exports = (robot) => {
-	robot.hear(/hello>/i, (msg) => {
-		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
-	});
+    robot.hear(/hello>/i, (msg) => {
+        let username = msg.message.user.name;
+        msg.send('Hello, ' + username);
+    });
+};
+module.exports = (robot) => {
+    robot.hear(/lot>/i, (msg) => {
+        const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+        const lot = lots[Math.floor(Math.random() * lots.length)];
+        let username = msg.message.user.name;
+        msg.send(lot + ',' + username);
+    });
 };
