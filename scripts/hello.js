b6f0d619af3bb009211b53c9e1087a02c8e6784b
jun110 <subaddress.jk110@gmail.com>
2018-09-17 06:26:10 +0000

    HubotとSlackアダプタの演習問題をコミット

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,10 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+	robot.hear(/lat>/i, (msg) => {
+		const lats = ['大吉', '中吉', '小吉', '凶']
+		const lat = lats[Math.floor(Math.random() * lats.length)]
+		const username = msg.message.user.name
+		msg.send(lat + ', ' + username)
+	})
 };
