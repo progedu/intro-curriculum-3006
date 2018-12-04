40e844496efa324ddde70322b5d42ad1ba913fad
Cessna-Key-Tree <shin1ho4@gmail.com>
2018-12-04 05:29:43 +0000

    占いを実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,17 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+	robot.hear(/lot>/i, (msg) => {
+		const lots = ['大吉', '中吉', '末吉', '吉', '凶'];
+		let i = Math.floor(Math.random() * lots.length);
+		const lot = lots[i];
+		const word = [
+			'いい日になるね！',
+			'いいことありそう！',
+			'いいことあるかも？',
+			'穏やかな一日',
+			'どんまい！'
+		]
+		msg.send(lot + ', ' + word[i]);
+	});
 };
