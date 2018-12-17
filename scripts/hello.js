711dfb23090768546e1de3383b6cf2621aba87e6
taroimo5454 <number600boy@gmail.com>
2018-12-17 15:19:56 +0000

    おみくじ機能の実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,12 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+
+	robot.hear(/lot>/i, (msg) => {
+		const username = msg.message.user.name;
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+		const lot = lots[Math.floor(Math.random() * lots.length)]
+		msg.send('Hello, ' + username);
+		msg.send(lot + ', @' + username);
+	});
 };
