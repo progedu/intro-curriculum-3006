5288c84d6f2d1fd795aa3fb0d08693c442042484
kouta Yoza <yozakou@gmail.com>
2019-01-04 14:49:25 +0000

    おみくじ_Hubot

--- scripts/hello.js
+++ scripts/hello.js
@@ -2,6 +2,12 @@
 module.exports = (robot) => {
 	robot.hear(/hello>/i, (msg) => {
 		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
+		msg.send('hello, @' + username);
+	});
+	robot.hear(/おみくじ>/i, (msg) => {
+		const username = msg.message.user.name;
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send('Hi, how ara you. @' + username + 'おみくじの結果 あなたは' + lot + 'です。');
 	});
 };
