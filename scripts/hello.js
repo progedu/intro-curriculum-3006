5e4d473b41772f759034a8316a383b8d158056eb
FlaxGitHub <kaito_18n1200525@nnn.ed.jp>
2019-01-09 02:56:38 +0000

    練習問題回答の訂正

--- scripts/hello.js
+++ scripts/hello.js
@@ -7,6 +7,7 @@ module.exports = (robot) => {
 	robot.hear(/lot>/i, (msg) => {
 		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
 		const lot = lots[Math.floor(Math.random() * lots.length)];
+		const username = msg.message.user.name;
 		msg.send(lot + ', @' + username);
 	});
 };
