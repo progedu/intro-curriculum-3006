4c0956f5da86d39750cac7da98520ea8cd0ab191
brazer496 <brazer496@gmail.com>
2019-01-30 12:29:01 +0900

    くじ引きを追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,16 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+
+	robot.hear(/lot>/i, (msg) => {
+		const username = msg.message.user.name;
+		// const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+		const lots = ['大吉', '吉', '中吉', '末吉',' 凶'];
+		
+		// const lot = lots[Math.floor(Math.random() * lots.length)];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		
+		// msg.send(lot + ', ' + username);
+		msg.send(username + 'は' + lot + 'を引きました！');
+	});
 };
