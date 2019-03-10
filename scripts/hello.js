ffcb40e6145798c43e619de4e49c6cce3fae6707
ksrnnb <enrico.fermi.phys@gmail.com>
2019-03-10 21:43:35 +0900

    くじ機能を追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,16 @@
 'use strict';
+
+const lots = ['大吉','吉', '中吉', '小吉', '凶'];
+
 module.exports = (robot) => {
-	robot.hear(/hello>/i, (msg) => {
-		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
-	});
+  robot.hear(/hello>/i, (msg) => {
+    const username = msg.message.user.name;
+    msg.send('Hello, ' + username);
+  });
+  robot.hear(/lot>/i, (msg) => {
+    const username = msg.message.user.name;
+    const lot = lots[parseInt(Math.random() * lots.length)];
+
+    msg.send(`${lot}, ${username}`);
+  });
 };
