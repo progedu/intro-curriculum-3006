c0f47dea43d0582183d3ce9c21d74273e47a0825
Daigoro <h.dai56@gmail.com>
2019-04-14 18:54:54 +0000

    ,の追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,6 +4,6 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		const lots = ["大吉","吉","中吉","末吉","凶"];
 		const lot = lots[Math.floor(Math.random() * lots.length)];
-		msg.send('Hello, ' + lot + username);
+		msg.send('Hello, ' + lot + "," + username);
 	});
 };
