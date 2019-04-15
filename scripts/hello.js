00b70f27246ab72da55e1425b746cda35b640a44
s-nishikoori <s-nishikoori@xware.co.jp>
2019-04-15 18:12:11 +0900

    運勢診断機能の追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,24 @@
-'use strict';
+`use strict`
 module.exports = (robot) => {
-	robot.hear(/hello>/i, (msg) => {
-		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
-	});
+    robot.hear(/hello>/i, (msg) => {
+        const username = msg.message.user.name;
+        msg.send('Hello, ' + username);
+    });
+
+    robot.hear(/lot>/i, (msg) => {
+        const username = msg.message.user.name;
+        const lots = ['大吉', '吉', '中吉',　'末吉',　'凶'];
+        const lot = lots[Math.floor(Math.random() * lots.length)];
+        if(lot === lots[0]){
+            msg.send(username + 'さんの運勢は……' + lot + 'だよ！宝くじ買いに行こう！');
+        }else if(lot === lots[1]){
+            msg.send(username + 'さんの運勢は……' + lot + 'だよ。いいことありそうだね。');
+        }else if(lot === lots[2]){
+            msg.send(username + 'さんの運勢は……' + lot + 'だよ。なかなかいい運勢だね。');
+        }else if(lot === lots[3]){
+            msg.send(username + 'さんの運勢は……' + lot + 'だよ。まあまあだね。');
+        }else{
+            msg.send(username + 'さんの運勢は……' + lot + 'だよ。まあこんなこともあるさ。');
+        }
+    })
 };
