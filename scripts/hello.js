01deb9ec7533c415c44ade41c29468b217f5f8f0
joeihata <j_ihata@uluru.jp>
2019-05-07 08:43:36 +0000

    練習問題

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,8 @@
 'use strict';
 module.exports = (robot) => {
-	robot.hear(/hello>/i, (msg) => {
-		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
+	robot.hear(/lot>/i, (msg) => {
+		const lots = ['大吉', '中吉', '吉', '末吉', '凶'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send(lot);
 	});
 };
