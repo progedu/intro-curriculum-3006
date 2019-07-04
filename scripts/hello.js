20dc940adfabc32bdf8c63681c45871e6fc5ab05
matchalol <rosequartz-elestial@msn.com>
2019-07-04 23:17:58 +0900

    lotの追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,24 @@
 'use strict';
+
+// module.exportsに関数やオブジェクトを登録
+// 他のスクリプトから呼び出せるようにする
 module.exports = (robot) => {
+
+	// hello>と入力したらHelloと返す
 	robot.hear(/hello>/i, (msg) => {
+		// 大文字小文字どちらでもマッチさせる
+		// マッチしたら無名関数を実行
+		// msgオブジェクトのプロパティ参照する
 		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
+		// ユーザー名の前に@をつけるとメンションを飛ばす
+		msg.send('Hello, @' + username);
 	});
+	// 練習問題のおみくじ
+	robot.hear(/lot>/i, (msg) => {
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		const username = msg.message.user.name;
+		msg.send(lot + ', @' + username);
+	});
+
 };
