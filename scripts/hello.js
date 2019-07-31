9dd5d3bad6ca8f826deb3c4a344aa2ed3fa3c237
ぱら <eriglai577@gmail.com>
2019-07-31 10:20:42 +0900

    おみくじ機能の実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -5,3 +5,9 @@ module.exports = (robot) => {
 		msg.send('Hello, ' + username);
 	});
 };
++	robot.hear(/lot>/i, (msg) => {
+		const username = msg.message.user.name;
+		const lots = ['大吉','吉','中吉','末吉','凶']
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send('あなたの運勢は、' + lot + username);
+	});
