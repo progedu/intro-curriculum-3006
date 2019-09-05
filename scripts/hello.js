cd350ee546bb582eed33ef30641699f897866129
竹内崇貴 <shukix124361@gmail.com>
2019-09-06 01:23:46 +0900

    修正

--- scripts/hello.js
+++ scripts/hello.js
@@ -8,7 +8,7 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
 		const lot = lots[Math.floor(Math.random() * lots.length)];
-		msg.send("lot, " + username);	
+		msg.send(lot + ", " + username);	
 	});
 };
 
