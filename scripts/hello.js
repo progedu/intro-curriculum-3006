058339509aaa8c1bd4f7c5df4cfa5bb770682062
davideryu2019test1 <davide.0829+2019test1@gmail.com>
2019-09-10 20:33:44 +0900

    おみくじ機能の実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,11 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+
+	robot.hear(/おみくじ/i, (msg) => {
+		const username = msg.message.user.name;
+		const lots = ['カレー', '餃子', 'ラーメン', 'チャーハン', '焼き肉', 'うどん'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send(`今日のごはんは ${lot} です！ @${username}`);
+	});
 };
