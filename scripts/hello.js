9d669b223aefa8b7314d449642f04930851fb065
moto-star <aruimi.okasii@gmail.com>
2019-09-16 19:37:52 +0900

    おみくじ機能を実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,11 +4,12 @@ const lot = lots[Math.floor(Math.random() * lots.length)];
 module.exports = (robot) => {
 	robot.hear(/hello>/i, (msg) => {
 		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
+		msg.send(`Hello,  @${username}`);
 	});
 
-	robot.hear(/うんせい/i, (msg) => {
+	robot.hear(/おみくじ/i, (msg) => {
 		const username = msg.message.user.name;
+		
 		msg.send(`@${username}さんの今日のおみくじは${lot}`);
 	});
 };
