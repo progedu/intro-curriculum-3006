debecda9852a80be56a0dc3b75bdf7ac655e03d9
AIOI <aioi18223@yahoo.co.jp>
2019-09-18 07:50:29 +0900

    おみくじ機能の実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,10 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+	robot.hear(/おみくじ/i, (msg) => {
+        const user_id = msg.message.user.id;
+        const lots = ['大吉です','吉です','中吉です','末吉です','凶です']
+        const lot = lots[Math.floor(Math.random() * lots.length)];
+        msg.send(`${lot}, <@${user_id}>`);
+    });
 };
