39effb8d6e5468ef1dc29d7a52a25b8d2cc3cb33
yabuchany <yabuchany@gmail.com>
2019-09-21 03:09:32 +0000

    おみくじ機能の実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,19 @@
-'use strict';
-module.exports = (robot) => {
-	robot.hear(/hello>/i, (msg) => {
-		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
-	});
+"use strict";
+module.exports = robot => {
+  robot.hear(/hello>/i, msg => {
+    const user_id = msg.message.user.id;
+    msg.send(`Hello, <@${user_id}>`);
+  });
+//   robot.hear(/おみくじ/i, msg => {
+//     const user_id = msg.message.user_id;
+//     const lots = ["カレー", "餃子", "ラーメン", "チャーハン", "うどん"];
+//     const lot = lots[Math.floor(Math.random() * lots.length)];
+//     msg.send(`今日のご飯は${$lot}です！,<@{user_id}>`);
+//   });
+  	robot.hear(/おみくじ/i, msg => {
+      const user_id = msg.message.user.id;
+      const lots = ["大吉", "吉", "中吉", "末吉", "凶"];
+      const lot = lots[Math.floor(Math.random() * lots.length)];
+      msg.send(`${lot}, <@${user_id}>`);
+    });
 };
