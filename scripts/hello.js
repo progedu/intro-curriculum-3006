e0cef5eadab3f335b705eb5db7ed3372690a0d59
necomamma <necomammajp@yahoo.co.jp>
2019-11-10 13:02:54 +0000

    おみくじ実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,11 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+	robot.hear(/おみくじ/, (msg) => {
+		const username = msg.message.user.name;
+		const lots = ['大吉', '中吉', '吉', '末吉', '凶']
+		const lot = lots[ Math.floor(Math.random() * lots.length)];
+		msg.send( username + 'さん、今日の運勢は' + lot + 'です。');
+	});
 };
+
