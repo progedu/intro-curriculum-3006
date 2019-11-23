bb41460c53174c2297e61bdf55747f7695df0986
lily live <hiroyukimaru5@gmail.com>
2019-11-23 23:20:06 +0900

    追加で一部修正しました。

--- scripts/hello.js
+++ scripts/hello.js
@@ -2,12 +2,12 @@
 module.exports = (robot) => {
 	robot.hear(/hello>/i, (msg) => {
 		const user_id = msg.message.user.id;
-		msg.send(`Hello, <@＄{user_id}>`);
+		msg.send(`Hello, <@${user_id}>`);
 	});
 	robot.hear(/おみくじ/i, (msg) => {
 		const user_id = msg.message.user.id;
 		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
 const lot = lots[Math.floor(Math.random() * lots.length)];
-msg.send(`${lot}, <@＄{user_id}>`);
+msg.send(`${lot}, <@${user_id}>`);
 	});
 };
