4ded574018b56948ecca9a9c815c6de50a49067f
Taiga Ozawa <taigaprog@gmail.com>
2020-01-04 23:46:34 +0900

    おみくじを実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,14 @@
 'use strict';
 module.exports = (robot) => {
 	robot.hear(/hello>/i, (msg) => {
+		const userid = msg.message.user.id;
+		msg.send('Hello, ' + `<@${userid}>`);
+		});
+
+	robot.hear(/おみくじ/i, (msg) => {
 		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
-	});
+		const lots = ['a', 'b', 'c'];
+		const lot = lots[Math.floor(Math.random()*lots.length)];
+		msg.send(`${lot}, ${username}`);
+		});
 };
