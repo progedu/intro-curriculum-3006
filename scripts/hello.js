ca26fc58208a53493e34fe8ce1bcf77d22d54378
Tee <milk06pop.12popken@gmail.com>
2020-01-10 12:05:11 +0900

    おみくじBot

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,12 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+
+	robot.hear(/おみくじ/i,(msg) =>{//マッチしたら関数実行
+		const user_id = msg.message.user.id
+		const lots = ['大吉','吉','中吉','末吉','凶','大凶']; //変数宣言
+		const lot = lots[Math.floor(Math.random() * lots.length)];// ランダム変数宣言
+		msg.send('${lot},<@${user_id}>'); //メンション
+
+	})
 };
