ed14d5821e08314c8ff61f6797d8da7e17d3249e
airotra <randomfractalcell@gmail.com>
2020-01-15 21:44:49 +0900

    user_id修正

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,13 +1,13 @@
 'use strict';
 module.exports = (robot) => {
 	robot.hear(/hello>/i, (msg) => {
-		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
+		const user_id = msg.message.user.id;
+		msg.send(`Hello,  <@${user_id}>`);
 	});
 	robot.hear('おみくじ', (msg) => {
 		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
 		const lot = lots[Math.floor(Math.random() * lots.length)];
-		const username = msg.message.user.name;
-		msg.send(lot + ', ' + username);
+		const user_id = msg.message.user.id;
+		msg.send(`${lot},  <@${user_id}>`);
 	});
 };
