9281886df5cd61c34161817e546311571cb28ec3
marimomarimoc <marimomarimo.biz@gmail.com>
2020-01-23 14:24:24 +0900

    夕飯おみくじ機能の実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,10 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+	robot.hear(/おみくじ/i, (msg) => {
+    const user_id = msg.message.user.id;
+    const lots = ['アボカド', '納豆', 'ラーメン', '豆乳鍋', 'カツカレー'];
+    const lot = lots[Math.floor(Math.random() * lots.length)];
+    msg.send(`<@${user_id}>さん、今夜は${lot}食べましょう！`);
+  });
 };
