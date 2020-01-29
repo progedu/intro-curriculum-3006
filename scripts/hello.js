17823ddff9868b813db48a1da1f180d20914a88e
hashimoto <h.hashimoto.web@gmail.com>
2020-01-29 06:00:52 +0000

    add Hello

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,5 +1,9 @@
 'use strict';
 module.exports = (robot) => {
+  robot.hear(/hello>/i, (msg) => {
+    const user_id = msg.message.user.id;
+    msg.send(`Hello, <@${user_id}>`);
+  });
 	robot.hear(/おみくじ>/i, (msg) => {
       const username = msg.message.user.name;
       const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
