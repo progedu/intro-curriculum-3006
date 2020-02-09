428c23a573ae5e8bb937e1d272ead3b3a2c05dbf
lvflwr <mayhem123757@gmail.com>
2020-02-09 16:22:26 +0000

    おみくじボットを実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,11 @@
 'use strict';
+
+const lots = ['大吉', '中吉', '吉', '凶', '大凶'];
+
 module.exports = (robot) => {
-	robot.hear(/hello>/i, (msg) => {
-		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
-	});
+  robot.hear(/おみくじ/, (msg) => {
+	const username = msg.message.user.name;
+	const lot = lots[Math.floor(Math.random() * lots.length)];
+    msg.send(`${lot}, ${username}`);
+  });
 };
