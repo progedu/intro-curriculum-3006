9203910faea4d6eeba4fdf100048db3c4cb1ff52
こなちゃん <conachan1125@gmail.com>
2020-03-01 15:20:50 +0900

    ミスの修正

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,15 +1,15 @@
 'use strict';
+
 module.exports = (robot) => {
 	robot.hear(/hello>/i, (msg) => {
 		const username = msg.message.user.name;
-		msg.send('こんにちは、' + username);
+		msg.send(`Hello, <@${username}>`);
 	});
-};
-module.exports = (robot) => {
+
 	robot.hear(/おみくじ/i, (msg) => {
 		const username = msg.message.user.name;
 		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
 		const lot = lots[Math.floor(Math.random() * lots.length)];
-		msg.send(lot + ',' + username);
+		msg.send(`${lot}, <@${username}>`);
 	})
-}
+};
