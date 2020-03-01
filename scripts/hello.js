8bead2194c8863bab13512c2081112ac324109e9
こなちゃん <conachan1125@gmail.com>
2020-03-01 15:14:32 +0900

    ミスがあったので修正

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,9 +1,15 @@
 'use strict';
-const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
-const lot = lots[Math.floor(Math.random() * lots.length)];
 module.exports = (robot) => {
 	robot.hear(/hello>/i, (msg) => {
 		const username = msg.message.user.name;
-		msg.send(lot + ',' + username);
+		msg.send('こんにちは、' + username);
 	});
 };
+module.exports = (robot) => {
+	robot.hear(/おみくじ/i, (msg) => {
+		const username = msg.message.user.name;
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send(lot + ',' + username);
+	})
+}
