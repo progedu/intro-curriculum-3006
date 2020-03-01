6df670c79ff4022ed6df6f947c9a5b197a0cea7d
こなちゃん <conachan1125@gmail.com>
2020-03-01 15:07:45 +0900

    おみくじ機能の実装完了

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,9 @@
 'use strict';
+const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+const lot = lots[Math.floor(Math.random() * lots.length)];
 module.exports = (robot) => {
 	robot.hear(/hello>/i, (msg) => {
 		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
+		msg.send(lot + ',' + username);
 	});
 };
