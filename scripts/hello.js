69e0b47fc441dfddcee31a5126b35e11be09a891
channel-4 <ch4.ito.y@gmail.com>
2020-03-11 21:41:35 +0900

    [update]hubot用のスクリプトにおみくじ機能を追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,19 @@
 'use strict';
 module.exports = (robot) => {
 	robot.hear(/hello>/i, (msg) => {
-		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
+		const user_id = msg.message.user.id;
+		msg.send(`Hello, <@${user_id}>`);
+	});
+
+	robot.hear(/おみくじ/i, (msg) => {
+		// slackのユーザ名
+		const user_id = msg.message.user.id;
+
+		// ランダムでlotsの中から選択
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+	 
+		// 例) 大吉, {あなたのユーザー名}
+		msg.send(`${lot}, <@${user_id}>`);
 	});
 };
