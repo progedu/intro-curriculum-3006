613564414478584c2894f4f4ea94bb031164fb9e
Takuya <931shinotaku@gmail.com>
2020-03-11 03:51:33 +0000

    改善

--- scripts/hello.js
+++ scripts/hello.js
@@ -2,6 +2,12 @@
 module.exports = (robot) => {
 	robot.hear(/hello>/i, (msg) => {
 		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
+		msg.send(`Hello, ' + <@${username}>`);
+	});
+	robot.hear(/おみくじ/i, (msg) => {
+    const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+    const fortune = lots[Math.floor(Math.random()*lots.length)];
+    const user_id = msg.message.user.id;
+    msg.send(`${fortune} , <@${user_id}>`);
 	});
 };
