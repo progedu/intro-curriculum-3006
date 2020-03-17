6a4f83509045d0366805be98fc3913253271197f
ron0318 <ron0318@ezweb.ne.jp>
2020-03-17 23:30:32 +0900

    おみくじの実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -6,7 +6,7 @@ module.exports = (robot) => {
 	});
 	robot.hear(/おみくじ/i, (msg) => {
 		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
-		const lot = lots[Math.floor(Math.rondom() * lots.length)];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
 		msg.send(`${lot}, <@${uername}>`)
 	})
 };
