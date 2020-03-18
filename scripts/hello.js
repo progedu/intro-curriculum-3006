6a6d9f7f8a0b09662c421ae40b0a1f5f01f39038
bobson14 <ryosuke.ishikawa.1990@gmail.com>
2020-03-18 22:40:32 +0900

    おみくじbotを実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,14 @@
 'use strict';
 module.exports = (robot) => {
 	robot.hear(/hello>/i, (msg) => {
-		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
+		const user_name = msg.message.user.name;
+		msg.send('Hello, ' + user_name);
+	});
+
+	robot.hear(/おみくじ/i, (msg) => {
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		const user_name = msg.message.user.name;
+		msg.send(`${lot}, <@${user_name}>`);
 	});
 };
