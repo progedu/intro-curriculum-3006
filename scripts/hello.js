39cb434bdbcfc0245284cdb19798a82fc306dac0
nandemotoken <nandemotoken@gmail.com>
2020-03-24 23:50:44 +0900

    my answer

--- scripts/hello.js
+++ scripts/hello.js
@@ -2,6 +2,9 @@
 module.exports = (robot) => {
 	robot.hear(/hello>/i, (msg) => {
 		const username = msg.message.user.name;
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
 		msg.send('Hello, ' + username);
+		msg.send(lot);
 	});
 };
