c2d5776b00b20af669c0df080b221806517b431c
Thealth-n <ksnagano.77@gmail.com>
2020-03-27 16:47:53 +0900

    おみくじ機能の実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,8 +1,13 @@
 'use strict';
-const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
 module.exports = (robot) => {
+	robot.hear(/hello>/i, (msg) => {
+		console.log(msg);
+		const username = msg.message.user.name;
+		msg.send(`Hello, <@${username}>`);
+	});
 	robot.hear(/おみくじ/, (msg) => {
 		const username = msg.message.user.name;
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
 		const lot = lots[Math.floor(Math.random() * lots.length)];
 		msg.send(`${lot}, <@${username}>`);
 	});
