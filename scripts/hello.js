7d71240085aa5053edf7f5f5b1223a9f947f7518
souta1003 <sta.100100@gmail.com>
2020-03-27 22:34:12 +0900

    おみくじbot追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,14 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+	robot.hear(/おみくじ/i, (msg) => {
+		const user_id = msg.message.user.id;
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send(`${lot}.<@${user_id}>`);
+	});
+	robot.hear(/にゃーーん/i, (msg) => {
+		const user_id = msg.message.user.id;
+		msg.send(`にゃーーーーーん(=^・・^=) <@${user_id}>`);
+	});
 };
