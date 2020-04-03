06fe7e2eb4ca9441f89483e7cd2e451f47e3905e
gnotes3sound <isojin41@yahoo.co.jp>
2020-04-03 12:02:47 +0900

    おみくじ機能の実装 天気

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,10 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+	robot.hear(/明日の天気>/i, (msg) => {
+		const username = msg.message.user.name;
+		const lots = ['快晴', '晴れ', '曇り', '時時雨', '雨', '雷',　'ハリケーン',　'霧', '霜', '雪'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send(`明日の天気は ${lot} です！ @${username}`);
+	});
 };
