cb82e73173537b1fbdd823fe7c7da7f677cf4dae
HidakaRintaro <rintaro0411bus@gmail.com>
2020-04-06 22:55:03 +0900

    おみくじの実装完了

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,12 @@
 'use strict';
 module.exports = (robot) => {
 	robot.hear(/hello>/i, (msg) => {
-		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
-	});
-};
+		const user_id = msg.message.user.id;
+    msg.send(`Hello, <@${user_id}>`);
+		robot.hear(/おみくじ/i, (msg) => {
+			const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+			const lot = lots[Math.floor(Math.random() * lots.length)];
+			const user_id = msg.message.user.id;
+			msg.send(`${lot}, <@${user_id}>`);
+		});
+	};
