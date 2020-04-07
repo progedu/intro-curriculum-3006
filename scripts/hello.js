57503d9a19a1942a201a3213abcf7a504f6d3703
Sumi Suguru <sumi.suguru.723@gmail.com>
2020-04-07 22:48:29 +0900

    おみくじBotを作成しました

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,9 @@
 'use strict';
 module.exports = (robot) => {
-	robot.hear(/hello>/i, (msg) => {
+	robot.hear(/おみくじ/i, (msg) => {
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
 		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
+		msg.send(`${lot}, ${username}`);
 	});
 };
