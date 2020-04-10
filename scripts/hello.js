4e3868dd5da9feb5b67f6b4005fbe2434d74b253
Nagatel <susuwata613@gmail.com>
2020-04-10 11:49:59 +0900

    おみくじボットの実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,10 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+	robot.hear(/おみくじ/i, (msg) => {
+		const username = msg, message, username;
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+		const loy = lots[Math.floor(Math.random() * lots.length)];
+		msg.send(`${lot},<@${username}>`);
+	});
 };
