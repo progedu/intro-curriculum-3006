029cf1fcb0e8bff8ce17618d1729b8f0766c70af
Satoshi Niikura <niikura0703@gmail.com>
2020-04-18 08:40:22 +0900

    おみくじの回答

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,6 +1,10 @@
 'use strict';
 
 module.exports = (robot) => {
+	robot.hear(/hello>/i, msg => {
+		const user_id = msg.message.user.id;
+		msg.send(`Hello, <@${user_id}>`);
+	  });
 	robot.hear(/おみくじ/i, (msg) => {
 		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
 		const lot = lots[Math.floor(Math.random() * lots.length)];
