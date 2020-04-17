b90693226f0f115b5a12ab1b3d5ae24c6276f1ca
asato <63636723+r-301@users.noreply.github.com>
2020-04-17 18:39:05 +0900

    おみくじを追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -9,5 +9,5 @@ module.exports = (robot) => {
     const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
     const lot = lots[Math.floor(Math.random() * lots.length)];
 msg.send(lot , `<@${user_id}>`)
-});
+});　
 };
