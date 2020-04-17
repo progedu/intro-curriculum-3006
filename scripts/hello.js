f88c1b41c4011eaaa30930ef3e20e3254b6d5272
ka-penn <takeji.kybetutaro.12.6@gmail.com>
2020-04-17 12:16:11 +0900

    おみくじボット

--- scripts/hello.js
+++ scripts/hello.js
@@ -5,3 +5,11 @@ module.exports = (robot) => {
 		msg.send('Hello, ' + username);
 	});
 };
+const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+const lot = lots[Math.floor(Math.random() * lots.length)];
+module.exports = (robot) => {
+	robot.hear("おみくじ", (msg) => {
+		const username = msg.message.user.name;
+		msg.send(lot + username);
+	});
+};
