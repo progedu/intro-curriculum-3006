9bab42a53289da2af6002237ea30d45207face1e
ueda <2daken@gmail.com>
2020-04-22 18:42:28 +0900

    おみくじ機能の実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,11 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+
+	robot.hear(/おみくじ/i, (msg) => {
+		const userID = msg.message.user.id;
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send(`${lot}, <@${userID}>`);
+	});
 };
