78cd1d69af607bb2e6520e0207c4d38c8af7e7f7
kensuke natsui <k.natsui@freedom.co.jp>
2020-04-25 18:16:34 +0900

    おみくじ機能を追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -5,3 +5,12 @@ module.exports = (robot) => {
 		msg.send('Hello, ' + username);
 	});
 };
+
+module.exports = robot => {
+    robot.hear(/おみくじ/i, msg => {
+      const user_id = msg.message.user.id;
+      const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+      const lot = lots[Math.floor(Math.random() * lots.length)];
+      msg.send(`${lot}, <@${user_id}>`);
+    });
+  };
