49adfc1102a12216047ab315d012badcf1f86181
kensuke natsui <k.natsui@freedom.co.jp>
2020-04-25 18:27:08 +0900

    exports変更

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,13 +4,11 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
-};
 
-module.exports = robot => {
-    robot.hear(/おみくじ/i, msg => {
-      const user_id = msg.message.user.id;
-      const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
-      const lot = lots[Math.floor(Math.random() * lots.length)];
-      msg.send(`${lot}, <@${user_id}>`);
-    });
-  };
+	robot.hear(/おみくじ/i, msg => {
+		const user_id = msg.message.user.id;
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send(`${lot}, <@${user_id}>`);
+	  });
+};
