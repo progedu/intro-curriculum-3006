ebb43492e98fd931122504707cb620e28fe0d2d0
tk-code-dev <reiwa.dupuis.2019@gmail.com>
2020-04-27 23:38:47 +0000

    Implementation of omikuji function

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,14 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+	robot.hear(/bye>/i, (msg) => {
+		const username = msg.message.user.name;
+		msg.send('See you!, ' + username);
+	});
+	robot.hear(/omikuji/i, (msg) => {
+		const user_id = msg.message.user.id;
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send(`${lot}, <@${user_id}>`);
+	  });
 };
