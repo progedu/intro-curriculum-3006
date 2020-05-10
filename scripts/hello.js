625c2d38a2f0a79f1f6aebfb38cb865eea033a57
kotomi-murayama <koccccchan5103@gmail.com>
2020-05-10 15:39:25 +0900

    typo

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,14 +1,14 @@
 'use strict';
 module.exports = (robot) => {
 	robot.hear(/hello>/i, (msg) => {
-		const username = msg.message.user.name;
-		msg.send('Hello, <@${user_id}>');
+		const user_id = msg.message.user.id;
+		msg.send(`Hello, <@${user_id}>`);
 	});
 	robot.hear(/おみくじ/i, (msg) => {
-		const username = msg.message.user.id;
+		const user_id = msg.message.user.id;
 		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
 		const lot = lots[Math.floor(Math.random() * lots.length)];
-		msg.send(`${lot}, <@$(user_id)>`);
+		msg.send(`${lot}, <@${user_id}>`);
 	});
 
 };
