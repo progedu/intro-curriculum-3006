332c903e53d0a63d93386e36b9e2b612689ece07
Masanobu Wakisaka <yurure32@gmail.com>
2020-05-20 16:31:28 +0900

    hello>とおみくじを修正

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,13 +1,13 @@
 'use strict';
 module.exports = (robot) => {
 	robot.hear(/hello>/i, (msg) => {
-		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
+		const user_id = msg.message.user.id;
+		msg.send(`Hello, <@${user_id}>`);
 	});
-	robot.hear(/おみくじ>/i, (msg) => {
-		const username = msg.message.user.name;
+	robot.hear(/おみくじ/i, (msg) => {
+		const user_id = msg.message.user.id;
 		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
 		const lot = lots[Math.floor(Math.random() * lots.length)];
-		msg.send(lot + username);
+		msg.send(`${lot}, <@${user_id}>`);
 	});
 };
