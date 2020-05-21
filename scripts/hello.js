5b625505dcbbca0fb056f3a2974baffc51da3046
tagamiwork <uedawork2020@gmail.com>
2020-05-21 15:01:42 +0900

    名前占い機能の実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -14,6 +14,39 @@ module.exports = (robot) => {
 		msg.send(`今日の運勢は${lot}です！@${username}`);
 	});
 
+	robot.hear(/名前占い>(.*)/i, (msg) => {
+		const username = msg.message.user.name;
+		const answers = [
+			'のいいところは声です。',
+			'のいいところはまなざしです。',
+			'のいいところは情熱です。',
+			'のいいところは厳しさです。',
+			'のいいところは知識です。',
+			'のいいところはユニークさです。',
+			'のいいところは用心深さです。',
+			'のいいところは見た目です。',
+			'のいいところは決断力です。',
+			'のいいところは思いやりです。',
+			'のいいところは感受性です。',
+			'のいいところは節度です。',
+			'のいいところは好奇心です。',
+			'のいいところは気配りです。',
+			'のいいところはその全てです。',
+			'のいいところは自制心です。',
+			'のいいところは優しさです。'
+		];
+		
+		let sumOfCharCode = 0;
+		for (let i = 0; i < username.length; i++) {
+			sumOfCharCode += username.charCodeAt(i);
+		}
+		
+		const index = sumOfCharCode % answers.length;
+		let result = answers[index];
+		msg.send(`${username}${result}`);
+	});
+
+
 };// module.exports
 
 /*　Math関数について
