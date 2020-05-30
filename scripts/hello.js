ca36e6f312adf36477eca9f3336ff2097c595cfc
anje3 <hiranoa0206@gmail.com>
2020-05-30 16:28:36 +0900

    おみくじ機能を追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,13 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+	robot.hear(/おみくじ/i, msg => {
+    const user_id = msg.message.user.id;
+    const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+    const lot = lots[Math.floor(Math.random() * lots.length)];
+    const films = ['ショーシャンクの空に', 'ライフ・イズ・ビューティフル', 'プラダを着た悪魔', 'キングスマン', 'ラ・ラ・ランド']
+    const film = films[Math.floor(Math.random() * films.length)];
+
+    msg.send(`${lot}でした！ 今日は ${film} がおすすめです。`);
+  });
 };
