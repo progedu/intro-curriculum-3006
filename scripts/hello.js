456efa66497a001991ef3fa2757f9f08f420ac01
ay-sugiura <bun.cedar@gmail.com>
2020-05-31 17:49:50 +0900

    ユーザIDに変更

--- scripts/hello.js
+++ scripts/hello.js
@@ -6,10 +6,10 @@ module.exports = (robot) => {
 	});
 
 	robot.hear(/おみくじ/, (msg) => {
-		const username = msg.message.user.name;
+		const userid = msg.message.user.id;
 		const lots = ['大吉','吉','中吉','末吉','凶'];
 		const lot = lots[Math.floor(Math.random() * lots.length)];
-		msg.send(lot +',' +  ${username});
+		msg.send(lot +',' +  userid);
 
 	});
 
