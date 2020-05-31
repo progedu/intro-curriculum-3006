15258d20cf0090c4d6896746233f8f37f4e22548
ay-sugiura <bun.cedar@gmail.com>
2020-05-31 17:54:06 +0900

    ユーザID、ユーザネームの違い

--- scripts/hello.js
+++ scripts/hello.js
@@ -6,10 +6,13 @@ module.exports = (robot) => {
 	});
 
 	robot.hear(/おみくじ/, (msg) => {
+		const username = msg.message.user.name;
 		const userid = msg.message.user.id;
 		const lots = ['大吉','吉','中吉','末吉','凶'];
 		const lot = lots[Math.floor(Math.random() * lots.length)];
-		msg.send(lot +',' +  userid);
+		msg.send(lot +', ユーザID' +  userid);
+		msg.send(lot +', ユーザネーム' +  username);
+		msg.send(lot +', @ユーザID' +  '<@' +  userid +'>');
 
 	});
 
