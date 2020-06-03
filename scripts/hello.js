2850ca6ed3cc02c30b66a14410817af1afce5806
enuyobi2020 <aamendokuse@hotmail.com>
2020-06-03 13:13:41 +0900

    おみくじを実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,13 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+
+	robot.hear(/おみくじ/i,(msg)=>{
+		const username = msg.message.user.name;
+        const lots=['大吉','吉','中吉','末吉','凶'];
+        const lot=lots[Math.floor(Math.random()*lots.length)];//Math.randomは０以上1未満の数をランダムで返す。
+        //これにlots.lengthすなわち５を掛け、Math.floorで小数点以下を切り捨てる。結果、0から4までの整数をランダムに得て、これを配列から選ぶことで運勢を得る。
+		//console.log(lot)
+		msg.send(`${username}様の今日の運勢は、${lot}です。`);
+    })
 };
