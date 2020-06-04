c9ca19289e4eff44510bae3d7439ad1e922cc81e
Rikiya Shogen <iphone.shogen@gmail.com>
2020-06-04 12:05:16 +0900

    おみくじ機能追加（修正版）

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,15 +1,14 @@
 'use strict';
-module.exports = (robot) => {
-	robot.hear(/hello>/i, (msg) => {
-		const username = msg.message.user.name;
-		omikuji();
-		msg.send(result + username);
+module.exports = robot => {
+    robot.hear(/hello>/i, msg =>{
+        const user_id = msg.message.user.id;
+        msg.send(`Hello, <@${user_id}>`);
 	});
+	
+    robot.hear(/おみくじ>/i, msg =>{
+		const user_id = msg.message.user.id;
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+        msg.send(`${lot}, <@${user_id}>`);
+    });
 };
-
-var result = null;
-function omikuji(){
-	const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
-	const lot = lots[Math.floor(Math.random() * lots.length)];
-	result = lot;
-}
