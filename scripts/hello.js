85f6464f3e653244cfe432a9d5e25196460d908b
Rikiya Shogen <iphone.shogen@gmail.com>
2020-06-04 11:56:35 +0900

    おみくじ機能追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -2,6 +2,14 @@
 module.exports = (robot) => {
 	robot.hear(/hello>/i, (msg) => {
 		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
+		omikuji();
+		msg.send(result + username);
 	});
 };
+
+var result = null;
+function omikuji(){
+	const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+	const lot = lots[Math.floor(Math.random() * lots.length)];
+	result = lot;
+}
