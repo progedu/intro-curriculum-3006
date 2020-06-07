6f3d777420ee8564303068b325ee0e09382f89c0
Harumi <hru.o6o4@gmail.com>
2020-06-07 15:08:30 +0900

    ラッキーカラーを教えてくれるbotの実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,21 @@
-'use strict';
+"use strict";
 module.exports = (robot) => {
-	robot.hear(/hello>/i, (msg) => {
-		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
-	});
+  robot.hear(/おみくじ/i, (msg) => {
+    const username = msg.message.user.name;
+    const lots = [
+      "赤",
+      "青",
+      "黄色",
+      "緑",
+      "茶色",
+      "黒",
+      "白",
+      "紫",
+      "オレンジ",
+      "ピンク",
+      "黄緑",
+    ];
+    const lot = lots[Math.floor(Math.random() * lots.length)];
+    msg.send(`今日のラッキーカラーは、${lot}です！ @${username}`);
+  });
 };
