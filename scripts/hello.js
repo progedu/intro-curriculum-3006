06fd31b630a4425112efc4e1fe2e2cd974030972
YamadaRyoya <yamada_ryoya@waku-2.com>
2020-07-14 14:17:20 +0900

    練習

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,11 @@
 'use strict';
+
+const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+
 module.exports = (robot) => {
-	robot.hear(/hello>/i, (msg) => {
+	robot.hear(/おみくじ/i, (msg) => {
+		const lot = lots[Math.floor(Math.random() * lots.length)];
 		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
+		msg.send(`${lot}, ${username}`);
 	});
 };
