687dc246bf85455b292fda0d8bbae945fb3db509
WataruToriumi <wataru0toriumi@gmail.com>
2020-07-15 23:16:25 +0900

    add omikuji

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,13 @@
 'use strict';
+const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
 module.exports = (robot) => {
 	robot.hear(/hello>/i, (msg) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
+	});
+	robot.hear(/おみくじ/, (msg) =>{
+		const lot = lots[Math.floor(Math.random()*lots.length)];
+		const userName = msg.message.user.name;
+		msg.send(lot + ', ' + userName);
 	});
 };
