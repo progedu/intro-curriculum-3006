0ff5f34460d647d31bda6278ce67599e08bf5134
qqlil <qqlilx3@gmail.com>
2020-08-30 16:46:43 +0900

    おみくじボット修正版

--- scripts/hello.js
+++ scripts/hello.js
@@ -10,5 +10,6 @@ module.exports = (omikuji) => {
 	omikuji.hear(/おみくじ/i, (omkj) => {
 		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
 		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send(`${lot}`);
 	});
 };
