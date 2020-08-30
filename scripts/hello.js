eba4b88d346787b9bbf092204cf808434fa7fa27
qqlil <qqlilx3@gmail.com>
2020-08-30 16:52:23 +0900

    おみくじボット修正版2

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,12 +4,10 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
-};
-
-module.exports = (omikuji) => {
-	omikuji.hear(/おみくじ/i, (omkj) => {
-		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
-		const lot = lots[Math.floor(Math.random() * lots.length)];
-		msg.send(`${lot}`);
-	});
+robot.hear(/おみくじ/i, msg => {
+	const user_id = msg.message.user.id;
+	const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+	const lot = lots[Math.floor(Math.random() * lots.length)];
+	msg.send(`${lot}, <@${user_id}>`);
+　});
 };
