5123bd5493e5090269bed0920a4a91065106041e
qqlil <qqlilx3@gmail.com>
2020-08-30 16:39:53 +0900

    おみくじボット

--- scripts/hello.js
+++ scripts/hello.js
@@ -5,3 +5,10 @@ module.exports = (robot) => {
 		msg.send('Hello, ' + username);
 	});
 };
+
+module.exports = (omikuji) => {
+	omikuji.hear(/おみくじ/i, (omkj) => {
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+	});
+};
