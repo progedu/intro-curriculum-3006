8b10c295379d8251f06274e275f0cc7409254a2f
HappyKumamoto <zen.word+hub@gmail.com>
2020-09-17 23:58:55 +0900

    おみくじ その2

--- scripts/hello.js
+++ scripts/hello.js
@@ -6,8 +6,14 @@ module.exports = robot => {
   });
   robot.hear(/おみくじ/i, msg => {
     const user_id = msg.message.user.id;
-    const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+    const lots = ['大吉', '吉', '中吉', '末吉', ];
     const lot = lots[Math.floor(Math.random() * lots.length)];
     msg.send(`${lot}, <@${user_id}>`);
+  });
+  robot.hear(/HappyDay/i, msg => {
+    const user_id = msg.message.user.id;
+    const lots = ['GOOD', 'NICE', 'GREAT', 'EXCELLENT','AMAZING','COOL','AWESOME','LOVELY' ];
+    const lot = lots[Math.floor(Math.random() * lots.length)];
+    msg.send(`Today is a ${lot} day! , <@${user_id}>`);
   });
 };
