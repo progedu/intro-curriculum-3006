ed8ca509a395d154b3f5dcc5deec6340911e5e0e
heavyfox <steamtrader@gmai.com>
2020-09-25 19:54:15 +0900

    おみくじ追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,24 @@
 'use strict';
 module.exports = (robot) => {
-	robot.hear(/hello>/i, (msg) => {
-		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
+
+	robot.hear(/hello>/i, msg => {
+        const user_id = msg.message.user.id;
+        msg.send(`hello 😆, <@${user_id}>`);
+    });
+	robot.hear(/hey siri/i, msg => {
+        const user_id = msg.message.user.id;
+        msg.send(`I'm not siri 🤔, <@${user_id}>`);
+    });
+	robot.hear(/おみくじ/i, msg => {
+		const user_id = msg.message.user.id;
+		const lots = ['大吉', '中吉', '小吉', '末吉', '凶', '大凶'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send(`${lot}, <@${user_id}>`);
+	});
+	robot.hear(/emoji/i, msg => {
+		const user_id = msg.message.user.id;
+		const lots = ['😀', '😖', '👺', '😱', '🤔', '💪'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send(`${lot}, <@${user_id}>`);
 	});
 };
