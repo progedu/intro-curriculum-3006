ee54e6efd562032ec3a8bdbc5f3d68f8432014c9
natumenonomi <nonegemu3e@gmail.com>
2020-09-25 20:46:19 +0900

    おみくじとじゃんけん追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,37 @@
 'use strict';
-module.exports = (robot) => {
-	robot.hear(/hello>/i, (msg) => {
-		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
+module.exports = robot => {
+  robot.hear(/hello>/i, msg => {
+    const user_id = msg.message.user.id;
+    msg.send(`Hello, <@${user_id}>`);
 	});
+	//おみくじ
+  robot.hear(/おみくじ/i, msg => {
+    const user_id = msg.message.user.id;
+    const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+    const lot = lots[Math.floor(Math.random() * lots.length)];
+    msg.send(`${lot}, <@${user_id}>`);
+	});
+	//じゃんけん
+	robot.hear(/じゃんけん/i, msg => {
+    const user_id = msg.message.user.id;
+    msg.send(`「グー」、「チョキ」、「パー」どれにする？, <@${user_id}>`);
+	});
+	robot.hear(/グー/i, msg => {
+    const user_id = msg.message.user.id;
+    const lots = ['グー！あいこです。もう一回！', 'チョキ！おめでとう！あなたの勝ち！', 'パー！私の勝ち。またチャレンジしてね',];
+    const lot = lots[Math.floor(Math.random() * lots.length)];
+    msg.send(`${lot}, <@${user_id}>`);
+	});
+	robot.hear(/チョキ/i, msg => {
+    const user_id = msg.message.user.id;
+    const lots = ['グー！私の勝ち。またチャレンジしてね', 'チョキ！あいこです。もう一回！', 'パー！おめでとう！あなたの勝ち！',];
+    const lot = lots[Math.floor(Math.random() * lots.length)];
+    msg.send(`${lot}, <@${user_id}>`);
+	});
+	robot.hear(/パー/i, msg => {
+    const user_id = msg.message.user.id;
+    const lots = ['グー！おめでとう！あなたの勝ち！', 'チョキ！私の勝ち。またチャレンジしてね', 'パー！あいこです。もう一回！',];
+    const lot = lots[Math.floor(Math.random() * lots.length)];
+    msg.send(`${lot}, <@${user_id}>`);
+  });
 };
