04cb9a85166892723f5ff77fb63d951f61112ffa
theata <yudengdaotian@gmail.com>
2020-09-25 20:06:57 +0900

    おみくじ機能を追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,12 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+
+	robot.hear(/おみくじ/i, msg => {
+		const user_id = msg.message.user.id;
+		const lots = ['大吉^_^','中吉','吉','小吉','末吉','凶T_T'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send(`あなたの運勢は${lot},<@${user_id}>`);
+	});
 };
+
