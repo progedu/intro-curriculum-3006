91d6e835cc9af65fbed66c62cf80a0c800cfd5c9
light Gears <180gears@gmail.com>
2020-09-26 11:30:38 +0900

    hubotでおみくじ作成

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,9 @@
 'use strict';
-module.exports = (robot) => {
-	robot.hear(/hello>/i, (msg) => {
-		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
-	});
+module.exports = robot => {
+  robot.hear(/こんにちは/i, msg => {
+		const user_id = msg.message.user.id;
+		const lots = ['大吉', '吉', '中吉', '末吉', '超大吉']
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+    msg.send(`${lot}, <@${user_id}>`);
+  });
 };
