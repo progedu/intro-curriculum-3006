4c43a5923f27b2fa4b34a2378060aa2555bb8933
light Gears <180gears@gmail.com>
2020-09-26 11:50:53 +0900

    入力を選択的代入に変更

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = robot => {
-  robot.hear(/こんにちは/i, msg => {
+  robot.hear(/[こんにちは|こんばんは|おはよう]/i, msg => {
 		const user_id = msg.message.user.id;
 		const lots = ['大吉', '吉', '中吉', '末吉', '超大吉']
 		const lot = lots[Math.floor(Math.random() * lots.length)];
