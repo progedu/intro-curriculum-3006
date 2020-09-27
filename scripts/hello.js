5f15b122b4fad532ced0d7c9b9714c8d29fee0ac
11hayato11 <2011mylife.322@gmail.com>
2020-09-27 18:30:28 +0900

    おみくじボット

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,14 @@
 'use strict';
 module.exports = (robot) => {
 	robot.hear(/hello>/i, (msg) => {
-		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
+		const user_id = msg.message.user.id;
+		msg.send(`Hello, <@${user_id}>`);
+	});
+	robot.hear(/おみくじ/i, (msg) => {
+		const user_id= msg.message.user.id
+		const omikujis = ["大吉","吉","中吉","末吉","凶"];
+		const omikuji = omikujis[Math.floor(Math.random() * omikujis.length)]
+		msg.send(`${omikuji}, <@${user_id}>`)
+
 	});
 };
