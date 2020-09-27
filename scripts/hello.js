3bb088a5e82bf3a7901d075c426e3153fbc52ee6
tomura yuki <techtom10m@gmail.com>
2020-09-27 14:51:29 +0900

    サイコロ機能の実装と機能の修正

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,16 +1,22 @@
 module.exports = (robot) => {
-	robot.hear(/hello>/i, (msg) => {
+	robot.hear(/サイコロ>/i, (msg) => {
 		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
+		const lots = ['1', '2', '3', '4', '5','6'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send(`サイコロの出目は` + lot + "です");
+	
 	});
-};
 
-module.exports = (robot) => {
 	robot.hear(/おみくじ>/i, (msg) => {
 		const username = msg.message.user.name;
 		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
 		const lot = lots[Math.floor(Math.random() * lots.length)];
 		msg.send(`今日の${username}の運勢は` + lot + "です");
 	
+	});
+
+	robot.hear(/hello>/i, (msg) => {
+		const username = msg.message.user.name;
+		msg.send('Hello, ' + username);
 	});
 };
