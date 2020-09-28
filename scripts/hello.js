6fee886d261747565b4e0b522612de5dd0ac5536
mayukuma <hoy_iam_temmie@icloud.com>
2020-09-28 16:39:35 +0900

    正しい答え

--- scripts/hello.js
+++ scripts/hello.js
@@ -8,5 +8,6 @@ module.exports = (robot) => {
     const user_id = msg.message.user.id;
     const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
 	const lot = lots[Math.floor(Math.random() * lots.length)];
+	msg.send(`${lot},<@{user_id}>`);//この行が入っていなかった
 	});
 };
