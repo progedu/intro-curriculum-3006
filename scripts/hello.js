66f01310d0b2d8b2d8239ef31243d77303185a44
Aozora Okada <bluesky125osmkk@gmail.com>
2020-09-28 10:42:43 +0900

    おみくじに大凶を追加。じゃんけんを追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -6,7 +6,13 @@ module.exports = robot => {
 	});
 	robot.hear(/おみくじ/i, msg => {
 		const user_id = msg.message.user.id;
-		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶', '大凶'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send(`${lot}, <@${user_id}>`);
+	});
+	robot.hear(/じゃんけん/i, msg => {
+		const user_id = msg.message.user.id;
+		const lots = ['グー', 'チョキ', 'パー'];
 		const lot = lots[Math.floor(Math.random() * lots.length)];
 		msg.send(`${lot}, <@${user_id}>`);
 	});
