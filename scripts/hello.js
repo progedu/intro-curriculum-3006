5216dd790f4f349198fa835018c6ab8af8c0d59a
shinnosuke nohara <kuwaharaarasi@gmail.com>
2020-09-29 11:25:40 +0900

    おみくじ機能を追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,6 +4,6 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
 		const lot = lots[Math.floor(Math.random() * lots.length)];
-		msg.send('Hello, ' + username + '今日の運勢は' + lots + 'です');
+		msg.send('Hello, ' + username + '。今日の運勢は' + lot + 'です');
 	});
 };
