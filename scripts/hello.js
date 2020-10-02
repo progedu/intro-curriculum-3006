e3def1de3ad880b33850405c0faf421038f65da4
iu84 <65646867+iu84@users.noreply.github.com>
2020-10-02 15:42:54 +0900

    Update hello.js

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,27 +1,13 @@
 'use strict';
-// module.exports = (robot) => {
-	// robot.hear(/hello>/i, (msg) => {
-		//  const user_id = msg.message.user.id;
-		//  msg.send('Hello, <@${user_id}>');
-	//  });
-// 
-	// robot.hear(/おみくじ/i, msg => {
-		// const user_id = msg.message.user.id;
-		// const lots = ['大吉 赤い物を食べよう', '吉 黄色の文具を持とう', '中吉 白い小物を使おう', '末吉 青い洋服を着よう', '凶 ールドのアクセサリーをつけよう'];
-		// const lot = lots[Math.floor(Math.random() * lots.length)];
-		// msg.send(`${lot}, <@${user_id}>`);
-	// });
-// };
-
 module.exports = robot => {
-	robot.hear(/hello>/i, msg => {
-		const user_id = msg.message.user.id;
-		msg.send(`Hello, <@${user_id}>`);
-	});
-	robot.hear(/おみくじ/i, msg => {
-		const user_id = msg.message.user.id;
-		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
-		const lot = lots[Math.floor(Math.random() * lots.length)];
-		msg.send(`${lot}, <@${user_id}>`);
-	});
+    robot.hear(/おはよう/i, msg => {
+        const user_id = msg.message.user.id;
+        msg.send(`もう昼だよ(笑), <@${user_id}>`);
+    });
+    robot.hear(/おみくじ/i, msg => {
+        const user_id = msg.message.user.id;
+        const lots = ['やったね　大吉だよ', 'よかったね　吉だよ', 'まあまあかな　中吉です', 'とりあえず　末吉ね', 'あらら　凶だ'];
+        const lot = lots[Math.floor(Math.random() * lots.length)];
+        msg.send(`${lot}, <@${user_id}>`);
+    });
 };
