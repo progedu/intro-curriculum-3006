3f76eb808c78d97d8aa54aded5f3ff395a895418
samurai <xcybza155@gmail.com>
2020-10-02 15:37:59 +0900

    おみくじ機能の追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,27 @@
 'use strict';
-module.exports = (robot) => {
-	robot.hear(/hello>/i, (msg) => {
-		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
+// module.exports = (robot) => {
+	// robot.hear(/hello>/i, (msg) => {
+		//  const user_id = msg.message.user.id;
+		//  msg.send('Hello, <@${user_id}>');
+	//  });
+// 
+	// robot.hear(/おみくじ/i, msg => {
+		// const user_id = msg.message.user.id;
+		// const lots = ['大吉 赤い物を食べよう', '吉 黄色の文具を持とう', '中吉 白い小物を使おう', '末吉 青い洋服を着よう', '凶 ールドのアクセサリーをつけよう'];
+		// const lot = lots[Math.floor(Math.random() * lots.length)];
+		// msg.send(`${lot}, <@${user_id}>`);
+	// });
+// };
+
+module.exports = robot => {
+	robot.hear(/hello>/i, msg => {
+		const user_id = msg.message.user.id;
+		msg.send(`Hello, <@${user_id}>`);
+	});
+	robot.hear(/おみくじ/i, msg => {
+		const user_id = msg.message.user.id;
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send(`${lot}, <@${user_id}>`);
 	});
 };
