fb017237de39dd40d6da61bc04398e42c3389b4f
Potechi Usushio <flygon88.bass@gmail.com>
2020-10-08 19:33:57 +0900

    おみくじを実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,15 +4,6 @@ module.exports = (robot) => {
 		const user_id = msg.message.user.id;
 		msg.send(`Hello, <@${user_id}>`);
 	});
-	// robot.respond(/(.*)$/i, (msg) => {
-	// 	const greeting = msg.message.text;
-	// 	if greeting is "こんにちは"
-	// 		msg.reply "お昼はもう食べましたか？"
-	// 	else if greeting is "おはよう"
-	// 		msg.reply "よい1日を"
-	// 	else if greeting is "こんばんは"
-	// 		msg.reply "一日お疲れさまでした"
-	// });
 	robot.hear(/おみくじ/i, (msg) => {
 		const user_id = msg.message.user.id;
 		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
