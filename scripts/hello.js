9b1267112c4d926d9f9c95956451868572998659
gomi <assassincreed0461-gta005@outlook.jp>
2020-10-12 23:59:47 +0900

    おみくじ機能を作りました。

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,17 @@
+// 挨拶
 'use strict';
 module.exports = (robot) => {
 	robot.hear(/hello>/i, (msg) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
+	});
+};
+// おみくじ
+module.exports = (robot) => {
+	robot.hear(/おみくじ/i, (msg) => {
+		const username = msg.message.user.name;
+		const lots = ['大吉', '吉', '中吉', '末吉', '区']
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send(username + 'の運勢は' + lot);
 	});
 };
