84a6897277277513c91b65fb57c3e588e2f81228
tbo-y <tobo-y@dwango.co.jp>
2020-10-20 19:01:42 +0900

    botを作成

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,16 @@
 'use strict';
 module.exports = (robot) => {
 	robot.hear(/hello>/i, (msg) => {
-		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
+		const user_id = msg.message.user.id;
+		msg.send(`Hello, <@${user_id}>`);
 	});
 };
+module.exports = (robot) => {
+	robot.hear(/おみくじ/i, (msg) => {
+		const user_id = msg.message.user.id;
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send(`${lot}, <@${user_id}>`);
+	});
+};
+
