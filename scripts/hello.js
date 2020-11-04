e0b37beac9c10d9fcca8c7eb88d64c7e0d4b8a82
masa1993 <70550130+masa1993@users.noreply.github.com>
2020-11-04 11:32:23 +0900

    Update hello.js

--- scripts/hello.js
+++ scripts/hello.js
@@ -8,7 +8,7 @@ module.exports = (robot) => {
    robot.hear(/おみくじ>/i, (msg) => {
      const username = msg.message.user.name;
      const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
-const lot = lots[Math.floor(Math.random() * lots.length)];
+     const lot = lots[Math.floor(Math.random() * lots.length)];
      msg.send(lot, + username);
   });
 };
