6fee7f975bf2915d49c8556f6d999371cc08a88e
Ayumu Sasaki <ayumu_19n1103647@nnn.ed.jp>
2020-11-15 19:46:45 +0900

    間違いを修正

--- scripts/hello.js
+++ scripts/hello.js
@@ -5,6 +5,7 @@ module.exports = (robot) => {
 		msg.send(`Hello, <@${user_id}>`);
 	});
 	robot.hear(/おみくじ/i, msg => {
+		const user_id = msg.message.user.id;
 		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
 		const lot = lots[Math.floor(Math.random() * lots.length)];
 		msg.send(`${lot}, <@${user_id}>`);
