0bee6564448c25aa03dc6b0805b3da9bfa3a6e0b
yusuke <yuusuke_kotani@dwango.co.jp>
2020-12-04 16:14:23 +0900

    added omikuji

--- scripts/hello.js
+++ scripts/hello.js
@@ -2,6 +2,9 @@
 module.exports = (robot) => {
 	robot.hear(/hello>/i, (msg) => {
 		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+    		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send('Hello, ' + username + "Here\'s your omikuji result!!!!" );
+		msg.send(lot + "!!!!")
 	});
 };
