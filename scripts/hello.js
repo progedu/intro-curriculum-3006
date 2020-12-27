3727e9007a3313d34c6775d81ef8cd5e42dbb777
beginnerfuji <teenagefunclub10000@gmail.com>
2020-12-27 15:24:30 +0900

    自分がメンションされるように変更

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,6 +4,6 @@ module.exports = (robot) => {
 		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
 		const lot = lots[Math.floor(Math.random() * lots.length)];
 		const username = msg.message.user.name;
-		msg.send('こんにちは。 ' + username + 'の運勢は' + lot + 'です。');
+		msg.send(`こんにちは。 @${username} の運勢は ${lot} です。`);
 	});
 };
