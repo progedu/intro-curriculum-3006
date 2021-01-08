30d7365edaf8560782d4e6c7c6ebcf5803a78e90
aozametatuki <akomatsu1224@gmail.com>
2021-01-08 16:20:05 +0900

    おみくじを出力するhubotの実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,18 @@
 'use strict';
+
 module.exports = (robot) => {
+	robot.hear(/おみくじ/i, (msg) => {
+		// 自分の名前を取得する
+		const username = msg.message.user.name;
+		//おみくじの結果を取得する
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+
+		msg.send(lot + `,<@${username}>`);
+	});
+
 	robot.hear(/hello>/i, (msg) => {
 		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
+		msg.send(`Hello, <@${username}>`);
 	});
 };
