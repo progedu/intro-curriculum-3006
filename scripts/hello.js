d22989126574642d05c64d8317007536d73a4f6a
Shinya Yano <ssiinnyyaa55@gmail.com>
2021-02-07 02:00:07 +0900

    おみくじ機能を追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,10 @@
 'use strict';
+const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+const lot = lots[Math.floor(Math.random() * lots.length)];
 module.exports = (robot) => {
-	robot.hear(/hello>/i, (msg) => {
-		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
+	robot.hear(/おみくじ/i, (msg) => {
+		const user_id = msg.message.user.id;
+
+		msg.send(lot + 'です, ' + `<@${user_id}>` + 'さん');
 	});
 };
