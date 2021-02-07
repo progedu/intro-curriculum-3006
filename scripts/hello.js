678045e79aaf321b823430631322bb660a1c7b21
Shinya Yano <ssiinnyyaa55@gmail.com>
2021-02-07 11:38:12 +0900

    消してしまった挨拶機能を復活

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,10 +1,14 @@
 'use strict';
-const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
-const lot = lots[Math.floor(Math.random() * lots.length)];
 module.exports = (robot) => {
+	robot.hear(/hello>/i, msg => {
+		const user_id = msg.message.user.id;
+		msg.send(`Hello, <@${user_id}>`);
+	  });
+
 	robot.hear(/おみくじ/i, (msg) => {
 		const user_id = msg.message.user.id;
-
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];		
 		msg.send(lot + 'です, ' + `<@${user_id}>` + 'さん');
 	});
 };
