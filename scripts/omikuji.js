4cc9b36cfd998ea60f1ffb4c7755c42a651019e9
Kamonabe <ryo79158420k@icloud.com>
2021-02-12 03:39:14 +0900

    練習課題

---
+++ scripts/omikuji.js
@@ -0,0 +1,9 @@
+'use strict';
+module.exports=(robot)=>{
+    robot.hear(/おみくじ/i,(msg)=>{
+        const lots=['大吉', '吉', '中吉', '末吉', '凶'];
+        const lot=lots[Math.floor(Math.random() * lots.length)];
+        const user_id=msg.message.user.name;
+        msg.send(`${lot}, <@${user_id}>`);
+    })
+}
