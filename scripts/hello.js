6c52332c9bbebc7fc601d8fc3b35019ea692361f
けーさん <exceaz13cazie@gmail.com>
2021-02-23 20:00:36 +0900

    おみくじ機能を追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -2,6 +2,13 @@
 module.exports = (robot) => {
 	robot.hear(/hello>/i, (msg) => {
 		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
+		msg.send(`こんにちは、<@${username}`);
+	});
+
+	robot.hear(/おみくじ>/i, (msg) => {
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		const username = msg.message.user.name;
+		msg.send(`${lot},<@${username}>`);
 	});
 };
