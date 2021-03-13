86679198f68ea9371dfd170188e52560c7593501
Selno <meitxyps385@gmail.com>
2021-03-13 14:38:27 +0900

    ok googleと呼びかけると反応するようになりました

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,12 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+	robot.hear(/おみくじ/i, (msg) => {
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send(lot);
+	});
+	robot.hear(/ok google/i, (msg) => {
+		robot.send('はい、なんでしょ...ってちゃうわ！！！');
+	})
 };
