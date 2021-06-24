df80b256a9e0a32f35738a1084308a7209b3b97f
K K <ksrlist@gmail.com>
2021-06-25 01:21:16 +0900

    おみくじ機能を修正

--- scripts/hello.js
+++ scripts/hello.js
@@ -7,6 +7,12 @@ module.exports = (robot) => {
 		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
 		const lot = lots[Math.floor(Math.random() * lots.length)];
 
-		msg.send(lot+',' + username);
+		msg.send(lot + ',' + username);
 	});
-};
+
+	robot.hear(/hello>/i, msg => {
+		const user_id = msg.message.user.id;
+		msg.send(`Hello, <@${user_id}>`);
+	});
+
+}; 
