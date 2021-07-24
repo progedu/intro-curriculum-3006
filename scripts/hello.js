43dd67b96bcb7ab45068329bf37038e14f27c66e
Ayuzawa Yoshiaki <blacktomos@hotmail.com>
2021-07-24 11:36:22 +0900

    課題作成

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,11 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+	robot.hear(/おみくじ>/i, (msg) => {
+		const user_id = msg.message.user.id;
+		const lots = ['大吉','吉','中吉','末吉','凶','背中にしがみつく見知らぬ女が「お前の背中を一生離れない」と言うのだ']
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send(`${lot}, <@${user_id}`);
+	});
+
 };
