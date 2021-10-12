7cdfeda5c52cc3026268dfc7629f944ed4b2ed12
Mitsuo Sato <mitti310@gmail.com>
2021-10-12 14:34:52 +0900

    練習問題実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,13 +1,13 @@
 'use strict';
 module.exports = robot => {
-    robot.hear(/hello)/i, msg => {
-        const user_id = msg.message.user.id;
-        msg.send(`こんにちは,<@A${user_id}>`);
+    robot.hear(/hello>/i, msg => {
+        const username = msg.message.user.name;
+        msg.send('Hello, ' + username);
     });
-    robot.hear(/おみくじ>/i, msg => {
-        const user_id = msg.message.user.id;
+    robot.hear(/おみくじ/i, msg => {
+        const username = msg.message.user.name;
         const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
         const lot = lots[Math.floor(Math.random() * lots.length)];
-        msg.send(`${lots}, <@${user_id}`)
+        msg.send(`${lot}, ${username}`);
     });
 };
